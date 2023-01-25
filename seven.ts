1cd7744c4267c7140b68b18298ee59e95478851b
KoreanCodingMachine <pon03078@naver.com>
2023-01-26 00:14:20 +0900

    f

--- seven.ts
+++ seven.ts
@@ -1,3 +1,5 @@
+{
+
 
 // unknown일때를 제외하고는 as를 거의 사용하지 않는다. -> 타입가드 사용 
 
@@ -32,6 +34,70 @@ function numOrNumArr(a: number | number[]) {
   }
 
 // class는 그 자체로 타입의 이름이 될 수 있다. 
-class A
+// 하지만 class의 instance를 의미한다. 
+
+class A {
+    aaa() {}
+}
+
+class B {
+    bbb() {}
+}
 
 
+function aOrB(param:A | B){
+    if(param instanceof A){
+        param.aaa()
+    }else{
+        param.bbb()
+    }
+}
+
+aOrB(new A())
+aOrB(new B())
+
+
+}
+
+
+{
+
+    // 객체간의 타입 검사 서로 차이를 만들어줘야함 
+    // type이 다름 , 속성의 이름이 다름 
+
+    type B = { type: 'b', bbb: string };
+    type C = { type: 'c', ccc: string };
+    type D = { type: 'd', ddd: string };
+    type A = B | C | D;
+    
+    // 값으로 구분하는 경우
+    function typeCheck(a: A) {
+        if (a.type === 'b') {
+            a.bbb;
+        } else if (a.type === 'c') {
+            a.ccc;
+        } else {
+            a.ddd;
+        }
+    }
+
+    // 속성으로 구분하는 경우 
+    function typeCheck2(a:A){
+        if('bbb' in a){
+            a.bbb
+        } else if ('ccc' in a){
+            a.ccc
+        } else {
+            a.ddd
+        }
+    }
+
+
+    // 객체 생성이 type을 넣는 습관을 기르는게 좋음 
+
+    const human = {type:'human'}
+    const dog = {type:'dog'}
+    const cat = {type:'cat'}
+
+
+}
